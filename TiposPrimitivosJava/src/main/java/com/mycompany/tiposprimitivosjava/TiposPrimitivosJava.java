9dc6283c9611f20213c9745ccae072d7aa4603e4
IxVerdi1518 <estebanfe400@gmail.com>
2023-05-25 10:35:09 -0500

    Carga del proyecto

---
+++ TiposPrimitivosJava/src/main/java/com/mycompany/tiposprimitivosjava/TiposPrimitivosJava.java
@@ -0,0 +1,40 @@
+
+package com.mycompany.tiposprimitivosjava;
+
+public class TiposPrimitivosJava {
+
+    public static void main(String[] args) {
+        System.out.println("Tipos primitivos enteros: byte, short, int, long");
+        System.out.println("TABLA DE RANGOS");
+        System.out.println("BYTE:");
+        System.out.println("Valor minimo:\t" + Byte.MIN_VALUE);
+        System.out.println("Valor maximo:\t" + Byte.MAX_VALUE);
+        
+        
+        System.out.println("\n SHORT:");
+        System.out.println("Valor minimo:\t" + Short.MIN_VALUE);
+        System.out.println("Valor maximo:\t" + Short.MAX_VALUE);
+        
+        
+        System.out.println("\n INT:");
+        System.out.println("Valor minimo:\t" + Integer.MIN_VALUE);
+        System.out.println("Valor maximo:\t" + Integer.MAX_VALUE);
+        
+        
+        System.out.println("\n LONG:");
+        System.out.println("Valor minimo:\t" + Long.MIN_VALUE);
+        System.out.println("Valor maximo:\t" + Long.MAX_VALUE);
+        System.out.println("\nTipos primitivos enteros: float, double");
+        
+        
+        System.out.println("TABLA DE RANGOS");
+        System.out.println("FLOAT:");
+        System.out.println("Valor minimo:\t"+Float.MIN_VALUE);
+        System.out.println("Valor maximo:\t"+Float.MAX_VALUE);
+        
+        
+        System.out.println("\nDOUBLE");
+        System.out.println("Valor Minimo:\t"+Double.MIN_VALUE);
+        System.out.println("Valor maximo:\t"+Double.MAX_VALUE);
+    }
+}
